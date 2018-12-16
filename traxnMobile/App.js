20e070a7b338febf696622a21a9a172bdaf7441e
walkerSwift <walker@empirex.tech>
2018-12-16 13:55:22 -0500

    learning how to use different styles and components in RN

--- traxnMobile/App.js
+++ traxnMobile/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,24 +19,50 @@ const instructions = Platform.select({
 type Props = {};
 export default class App extends Component<Props> {
   state = {
-    placeName: ''
+    placeName: '',
+    places: []
   }
 
   placeNameChangedHandler = (val) => {
     this.setState({
       placeName: val
     });
-  }
+  };
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === "") {
+      return;
+    }
+
+    this.setState(prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName)
+      };
+    });
+  };
 
   render() {
+    const placesOutput = this.state.places.map((place, i) => (
+      <Text key={i}>{place}</Text>
+    ));
     return (
       <View style={styles.container}>
-        <TextInput 
-          style={{width: 300, borderColor: "black", borderWidth: 1}}
+      <View style={styles.inputContainer}>
+      <TextInput 
+          style={{ borderColor: "black", borderWidth: 1}}
           placeholder="input"
           value={this.state.placeName}
           onChangeText={this.placeNameChangedHandler}
+          style={styles.placeInput}
         />
+        <Button
+        title="Add"
+        style={styles.placeButton}
+        onPress={this.placeSubmitHandler} />
+      </View>
+      <View>
+        {placesOutput}
+      </View>
       </View>
     );
   }
@@ -45,18 +71,22 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 80,
+    justifyContent: 'flex-start',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  placeInput: {
+    width: '70%'
   },
+  placeButton: {
+    width: '30%'
+  }
 });
