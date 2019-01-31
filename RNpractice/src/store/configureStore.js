e3649b2789882a5956273500a40a8608fc015c81
walkerSwift <walker@empirex.tech>
2019-01-30 22:08:58 -0500

    added firebase

--- RNpractice/src/store/configureStore.js
+++ RNpractice/src/store/configureStore.js
@@ -1,5 +1,6 @@
 
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
 
 import placesReducer from './reducers/places';
 
@@ -14,7 +15,7 @@ if (__DEV__) {
 }
 
 const configureStore = () => {
-    return createStore(rootReducer, composeEnhancers);
+    return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
